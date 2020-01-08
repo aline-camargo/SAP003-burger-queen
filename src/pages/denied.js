5478d01f024531379ff3587d58e00fb3f2da6013
aline-camargo <aline97camargo@gmail.com>
2020-01-07 23:19:19 -0300

    pagina de acesso negado ok

--- src/pages/denied.js
+++ src/pages/denied.js
@@ -1,9 +1,94 @@
 import React from 'react';
+import { StyleSheet, css } from 'aphrodite/no-important';
+import { Link } from 'react-router-dom';
+import logo from '../images/bq.png';
 
 const Denied = () => {
-    return (
-        <h2>Acesso negado</h2>
-    );
+  return (
+    <main className={css(styles.div)}>
+      <section className={css(styles.section)}>
+        <div className={css(styles.message)}>
+          <h1 className={css(styles.title)}>Acesso negado</h1>
+          <p className={css(styles.text)}>
+            Você não tem permissão para<br></br>acessar essa página.
+          </p>
+          <p className={css(styles.text)}>
+            Volte à pagina anterior ou faça {''}
+            <Link to='/' className={css(styles.link)}>
+              login
+            </Link>
+            .
+          </p>
+        </div>
+      </section>
+      <img
+        src={logo}
+        className={css(styles.img)}
+        alt='Logo Burguer Queen'
+      ></img>
+    </main>
+  );
 };
+
+const styles = StyleSheet.create({
+  section: {
+    background: '#A62F03',
+    height: '100vh',
+    width: '80vw',
+    display: 'flex',
+    alignItems: 'center',
+    clipPath: 'polygon(0px 0px, 40vw 0px, 300vw 1100vw, 0px 100%)',
+    '@media (max-width: 975px)': {
+      clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0% 100%)',
+      width: '100vw',
+      height: '74vh'
+    }
+  },
+  div: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    '@media (max-width: 975px)': {
+      flexDirection: 'column-reverse'
+    }
+  },
+  text: {
+    fontSize: '1.1em',
+    color: 'white'
+  },
+  img: {
+    marginRight: '7.5%',
+    '@media (max-width: 414px)': {
+      height: '215px'
+    },
+    '@media (max-width: 975px)': {
+      marginRight: '0'
+    }
+  },
+  link: {
+    textDecoration: 'none',
+    color: 'rgb(225, 116, 9)',
+    transition: '0.2s linear',
+    ':hover': {
+      color: 'white'
+    }
+  },
+  title: {
+    color: 'white'
+  },
+  message: {
+    textAlign: 'center',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '65%',
+    height: '20%',
+    '@media (max-width: 975px)': {
+      width: '100%',
+      height: '50%'
+    }
+  }
+});
 
 export default Denied;
--- src/private.route.js
+++ src/private.route.js
@@ -1,56 +1,63 @@
 import React, { useEffect, useState } from 'react';
-import { useHistory } from "react-router-dom";
-import { Route, Redirect} from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import { auth, db } from './util/firebaseConfig';
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
-    let history = useHistory();
-    const [load, setLoad] = useState(true);
-    const [user, setUser] = useState(true);
-    
-    useEffect(() => {
-        auth.onAuthStateChanged((online) => {
-            if (online) {
-                db.collection('users').doc(online.uid)
-                .get().then(querySnapshot => {
-                    let kitchen = false;
+  // console.log(props)
+  let history = useHistory();
+  const [load, setLoad] = useState(true);
+  const [user, setUser] = useState(false);
 
-                    if(querySnapshot.data().kitchen) {
-                        kitchen = true;
-                    }
+  useEffect(() => {
+    auth.onAuthStateChanged(online => {
+      if (online) {
+        db.collection('users')
+          .doc(online.uid)
+          .get()
+          .then(querySnapshot => {
+            let kitchen = false;
 
-                    const kitchenAccess = Component.name === 'Kitchen' && kitchen;
-                    const saloonAccess = Component.name != 'Kitchen' && !kitchen;
-                                        
-                    if (kitchenAccess || saloonAccess){
-                        setUser(true)
-                    } else {
-                        setUser(false)
-                    }
-                    setLoad(false)
-                })
-            } else {
-                history.push('/negado')
+            if (querySnapshot.data().kitchen) {
+              kitchen = true;
             }
+
+            const kitchenAccess = Component.name === 'Kitchen' && kitchen;
+            const saloonAccess = Component.name !== 'Kitchen' && !kitchen;
+
+            if (kitchenAccess || saloonAccess) {
+              setUser(true);
+            } else {
+              setUser(false);
+            }
+            setLoad(false);
+            console.log('opa')
           });
+      } else {
+        setLoad(false);
+        setUser(false);
+      }
+    });
+  }, []);
 
-
-    }, []);
-
-
-    if (load) {
-        return null
-    } else {
-        return (
-            <Route {...rest} render={props => (
-                user ? (
-                <Component {...props} />
-                ) : (
-                <Redirect to={{pathname: '/negado', state: { from: props.location}}} />
-                )
-            )}/>
-        )
-    }
-}
+  if (load) {
+    return null;
+  } else {
+    return (
+      <Route
+        {...rest}
+        render={() =>
+          user ? (
+            <Component />
+          ) : (
+            <Redirect
+              to={{ pathname: '/negado', state: { from: rest.location } }}
+            />
+          )
+        }
+      />
+    );
+  }
+};
 
 export default PrivateRoute;
